71afeba4b4ead5c8237694c0b4b1a13f27deb0fe
mdelqq <josejoaquinescuderorodriguez@gmail.com>
2024-05-10 14:01:00 +0200

    Configuracion urls

--- mdelqq/urls.py
+++ mdelqq/urls.py
@@ -14,3 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('gecopf/', include('gecopf.urls')),
+    path('', RedirectView.as_view(url='/gecopf/', permanent=True)), #Add URL maps to redirect the base URL to our application
+    path('accounts/', include('django.contrib.auth.urls')), #Add Django site authentication urls (for login, logout, password management)
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
